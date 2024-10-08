f87c4cd28a33a82390f5d5fafe58bfdb5387f29b
Iulian Dragan <iulian_dragan@yahoo.com>
2024-10-08 08:40:58 +0200

    DESCRIPTION

--- tests/demo.R
+++ tests/demo.R
@@ -1,5 +1,13 @@
-get_Database
+devtools::install_github('TRIPLE-CHIST-ERA/VoIDR')
+library(VoIDR)
+makePackage('UniprotR','https://sparql.uniprot.org', voidEndpoint  = 'https://sparql.uniprot.org')
+
+
+library(UniprotR)
+get_Database()
+?get_Taxon
 x <- get_Taxon(properties = 'commonName',limit = 5000000)
+y <- get_Taxon(properties = "partOfLineage",limit = 5000000)
 y <- get_Taxon(properties = 'depiction',limit = 5000000)
 z <- expandDF(y)
 w <- inner_join(x,z)
