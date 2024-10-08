add5f90cb1c4c01db063f1dc07ba0976eed8d1fe
Iulian Dragan <iulian_dragan@yahoo.com>
2024-10-08 11:47:18 +0200

    demo

--- tests/demo.R
+++ tests/demo.R
@@ -2,20 +2,19 @@ devtools::install_github('TRIPLE-CHIST-ERA/VoIDR')
 library(VoIDR)
 
 makePackage('UniprotR','https://sparql.uniprot.org', voidEndpoint  = 'https://sparql.uniprot.org')
-
+install.packages('UniprotR_1.0.tar.gz')
 
 library(UniprotR)
+?get_Database
 get_Database()
 ?get_Taxon
-x <- get_Taxon(properties = 'commonName',limit = 5000000)
-y <- get_Taxon(properties = "partOfLineage",limit = 5000000)
-y <- get_Taxon(properties = 'depiction',limit = 5000000)
-z <- expandDF(y)
-w <- inner_join(x,z)
+x <- get_Taxon()
+x <- get_Taxon(properties = 'commonName',limit = 10000)
+y <- get_Taxon(properties = "partOfLineage",limit = 10000)
+z <- get_Taxon(properties = 'depiction',limit = 10000)
+w <- dplyr::inner_join(x,y)
+u <- dplyr::inner_join(w,z)
 
-
-get_Graph()
-get_Interaction()
-get_Interaction('participant')
-?get_Participant
-get_
+?get_Interaction()
+x <- get_Interaction('participant')
+expandDF(x)
