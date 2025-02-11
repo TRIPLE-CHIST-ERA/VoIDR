aea6139aa6a94742796f3500abebfde10bb3869f
Iulian Dragan <iulian_dragan@yahoo.com>
2025-02-11 11:45:07 +0100

    ...

---
+++ tests/demo2.R
@@ -0,0 +1,16 @@
+library(VoIDR)
+
+f <- makePackage('UniprotR','https://sparql.uniprot.org', voidEndpoint = 'http://localhost:7200/repositories/uniprotvoid')
+install.packages('./UniprotR_1.0.tar.gz')
+library(UniprotR)
+?get_Taxon
+x <- get_Taxon(c('mnemonic', 'commonName', 'strain'))
+str(x)
+x
+y <- get_Taxon(c('mnemonic','replaces', 'depiction'), only.complete.cases = TRUE, limit = NULL)
+
+?get_Protein
+get_Protein(c('complete', 'mnemonic', 'interaction'), only.complete.cases = TRUE, limit = NULL)
+
+get_Interaction(only.complete.cases = TRUE, limit = NULL)
+
